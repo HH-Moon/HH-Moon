9de02da93f5644b6c076a5e60fd4262f73dfbb7f
HHww__ <504293001@qq.com>
2025-02-04 11:53:49 +0800

    第二次提交 做出了主循环和背景图

--- 飞机大战/fly/fly.py
+++ 飞机大战/fly/fly.py
@@ -1,7 +1,18 @@
 import pygame
 
+#初始化界面
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('飞机大战')  #标题
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
+bgImg = pygame.image.load('bg.png')
+
+#游戏主循环
+running = True
+while running:
+    screen.blit(bgImg, (0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()

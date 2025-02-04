f1ed30abeb67eade77f5d04102c4a7d40b89a72b
HHww__ <504293001@qq.com>
2025-02-04 12:30:41 +0800

    第三次提交 实现了键盘上下左右控制飞机移动

--- 飞机大战/fly/fly.py
+++ 飞机大战/fly/fly.py
@@ -8,6 +8,12 @@ icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 bgImg = pygame.image.load('bg.png')
 
+playerImg = pygame.image.load('player.png')
+playerX = 380
+playerY = 500
+playerStepX = 0
+playerStepY = 0
+
 #游戏主循环
 running = True
 while running:
@@ -15,4 +21,31 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+#通过键盘控制飞机移动
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                playerStepX = 3
+            elif event.key == pygame.K_LEFT:
+                playerStepX = -3
+            elif event.key == pygame.K_UP:
+                playerStepY = -3
+            elif event.key == pygame.K_DOWN:
+                playerStepY = 3
+        if event.type == pygame.KEYUP:
+            playerStepX, playerStepY = 0, 0
+
+    screen.blit(playerImg, (playerX, playerY))
+    playerX += playerStepX
+    playerY += playerStepY
+
+    if playerX > 800 - 64:
+        playerX = 800 - 64
+    if playerX < 0:
+        playerX = 0
+    if playerY > 600 - 64:
+        playerY = 600 - 64
+    if playerY < 0:
+        playerY = 0
+
+
     pygame.display.update()
